b0ec4e8d3ced96a6cc00a892a8e4cb41e182e2dc
EduardoPari <163587399+EdMaker1@users.noreply.github.com>
2025-05-13 08:34:08 -0500

    Agrego la funcion que realizara con el boton como la descripcion del juego luego agrego parte de la interfaz inicial

--- app/src/main/java/com/example/myapplication/PestallaBienvenida.kt
+++ app/src/main/java/com/example/myapplication/PestallaBienvenida.kt
@@ -1,4 +1,37 @@
 package com.example.myapplication
 
-class PestallaBienvenida {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.InterfazBienvenidaBinding
+
+class PestallaBienvenida : Fragment() {
+
+    private var _vinculo: InterfazBienvenidaBinding? = null
+    private val vinculo get() = _vinculo!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _vinculo = InterfazBienvenidaBinding.inflate(inflater, container, false)
+        return vinculo.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // boton para ir al juego
+        vinculo.botonIniciar.setOnClickListener {
+            findNavController().navigate(R.id.action_bienvenidojuego)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _vinculo = null
+    }
 }
